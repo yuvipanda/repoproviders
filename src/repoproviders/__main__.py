0a4f3f2239dcb8f3ab139ce87043b5c3406f4db7
YuviPanda <yuvipanda@gmail.com>
2024-12-31 12:51:03 -0800

    Add a resolve subcommand

--- src/repoproviders/__main__.py
+++ src/repoproviders/__main__.py
@@ -6,21 +6,26 @@ from .resolvers import resolve
 
 async def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("question", help="What should we try to resolve?")
-    argparser.add_argument(
+
+    subparsers = argparser.add_subparsers(required=True, dest="command")
+    subparser = subparsers.add_parser("resolve")
+
+    subparser.add_argument("question", help="What should we try to resolve?")
+    subparser.add_argument(
         "--no-recurse",
         help="Do not recurse, return after first answer",
         action="store_true",
     )
     args = argparser.parse_args()
 
-    answers = await resolve(args.question, recursive=not args.no_recurse)
+    if args.command == "resolve":
+        answers = await resolve(args.question, recursive=not args.no_recurse)
 
-    if answers:
-        for a in answers:
-            print(a)
-    else:
-        print(f"Unable to resolve {args.question}")
+        if answers:
+            for a in answers:
+                print(a)
+        else:
+            print(f"Unable to resolve {args.question}")
 
 
 def cli_entrypoint():
