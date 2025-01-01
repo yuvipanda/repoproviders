da5993aa31b673400d575555e56b55bed4d841fd
YuviPanda <yuvipanda@gmail.com>
2025-01-01 10:55:56 -0800

    Move download_file into its own file

--- src/repoproviders/fetchers/dataverse.py
+++ src/repoproviders/fetchers/dataverse.py
@@ -5,27 +5,10 @@ import aiohttp
 from yarl import URL
 
 from ..resolvers.doi import DataverseDataset
+from ..utils import download_file
 
 
 class DataverseFetcher:
-    async def download_file(
-        self, session: aiohttp.ClientSession, url: URL, output_path: Path
-    ):
-        # Read in 4k chunks
-        CHUNK_SIZE = 4 * 1024
-        resp = await session.get(url)
-
-        resp.raise_for_status()
-
-        if not output_path.parent.exists():
-            # Leave the exist_ok=True here despite the check, to handle possible
-            # race conditions in the future if we ever parallelize this
-            output_path.parent.mkdir(parents=True, exist_ok=True)
-
-        with open(output_path, "wb") as f:
-            async for chunk in resp.content.iter_chunked(CHUNK_SIZE):
-                f.write(chunk)
-
     async def fetch(self, repo: DataverseDataset, output_dir: Path):
         files_url = repo.installationUrl / "api/datasets/:persistentId"
         files_url = files_url.with_query(persistentId=repo.persistentId)
@@ -49,6 +32,6 @@ class DataverseFetcher:
 
                 file_path = Path(os.path.join(f.get("directoryLabel", ""), file_name))
 
-                await self.download_file(
+                await download_file(
                     session, file_download_url, output_dir / file_path
                 )
---
+++ src/repoproviders/utils.py
@@ -0,0 +1,21 @@
+import aiohttp
+from yarl import URL
+from pathlib import Path
+
+async def download_file(
+    session: aiohttp.ClientSession, url: URL, output_path: Path
+):
+    # Read in 4k chunks
+    CHUNK_SIZE = 4 * 1024
+    resp = await session.get(url)
+
+    resp.raise_for_status()
+
+    if not output_path.parent.exists():
+        # Leave the exist_ok=True here despite the check, to handle possible
+        # race conditions in the future if we ever parallelize this
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(output_path, "wb") as f:
+        async for chunk in resp.content.iter_chunked(CHUNK_SIZE):
+            f.write(chunk)
