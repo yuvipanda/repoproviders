b7d904f1196e085a7ee54c7b550681efbaa146a3
YuviPanda <yuvipanda@gmail.com>
2025-01-01 19:22:38 -0800

    Clone git much faster with treeless commits

--- src/repoproviders/fetchers/git.py
+++ src/repoproviders/fetchers/git.py
@@ -1,19 +1,44 @@
 import asyncio
 from pathlib import Path
+from subprocess import CalledProcessError
 
 from ..resolvers.git import ImmutableGit
 
 
 class ImmutableGitFetcher:
     async def fetch(self, repo: ImmutableGit, output_dir: Path):
-        # FIXME: This isn't the most optimized clone but that's ok
 
-        # output_dir should be empty
-        command = ["git", "clone", "--recurse-submodules", repo.repo, str(output_dir)]
+        # Assume output_dir is empty
+        command = [
+            "git",
+            "clone",
+            "--filter=tree:0",
+            "--recurse-submodules",
+            repo.repo,
+            str(output_dir),
+        ]
         proc = await asyncio.create_subprocess_exec(
             *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
         stdout, stderr = [s.decode().strip() for s in await proc.communicate()]
         retcode = await proc.wait()
 
-        return
+        if retcode != 0:
+            # FIXME: Raise a more helpful error?
+            raise CalledProcessError(retcode, command, stdout, stderr)
+
+        command = ["git", "checkout", repo.ref]
+
+        proc = await asyncio.create_subprocess_exec(
+            *command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=str(output_dir)
+        )
+
+        stdout, stderr = [s.decode().strip() for s in await proc.communicate()]
+        retcode = await proc.wait()
+
+        if retcode != 0:
+            # FIXME: Raise a more helpful error?
+            raise CalledProcessError(retcode, command, stdout, stderr)
